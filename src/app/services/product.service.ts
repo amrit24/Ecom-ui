aba0105e8713c1c9a0f7838fdefc2c235fa7fe2a
amrit <amrit.shrivastava24@gmail.com>
2021-01-29 22:27:46 +0530

    Add urls

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,9 +10,9 @@ import { ProductCategory } from '../common/product-category';
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = ' http://ecomservice2-env.eba-xgc4qsgn.us-east-2.elasticbeanstalk.com/api/products';
 
-  private categoryUrl = 'http://localhost:8080/api/product-category';
+  private categoryUrl = 'http://ecomservice2-env.eba-xgc4qsgn.us-east-2.elasticbeanstalk.com/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
