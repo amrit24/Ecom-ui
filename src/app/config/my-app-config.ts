be6b24c69a4a8e261443918915e2be6719bf5be7
amrit <amrit.shrivastava24@gmail.com>
2021-02-24 22:13:16 +0530

    Prepare for deployment

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa4wgzdqyyr9Mk6d5d6',
         issuer: 'https://dev-38322652.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://ecom-first.herokuapp.com/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
