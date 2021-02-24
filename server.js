04b01dd83cfe1d024be946dbda1a3ac75ba5e309
amrit <amrit.shrivastava24@gmail.com>
2021-02-24 23:07:19 +0530

    fix root path

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ecom-ui'));
+app.use(express.static('./dist/angular-ecommerce'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ecom-ui'}),
+    res.sendFile('index.html', {root: 'dist/angular-ecommerce'}),
 );
 
 // Start the app by listening on the default Heroku port
