bdb870687e053619a3ecd59e5b8b18e7bc707669
amrit <amrit.shrivastava24@gmail.com>
2021-02-25 22:53:48 +0530

    Change routes

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa4wgzdqyyr9Mk6d5d6',
         issuer: 'https://dev-38322652.okta.com/oauth2/default',
-        redirectUri: 'https://ecom-first.herokuapp.com/login/callback',
+        redirectUri: 'https://ecomkart.herokuapp.com/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
